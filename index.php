c40c75f53779d590e20b1c8876d0333a0a6508bd
MisanthropeGirl <mail@clarissadarling.me.uk>
2015-02-08 19:34:18 +0000

    Moving to use the BEM approach to styling

--- index.php
+++ index.php
@@ -12,199 +12,193 @@
 	</head>
 
 	<body>
-		<div>
-			<header>
+		<div class="layout">
+			<header class="layout__header">
 				<h1>Clarissa Clement</h1>
 			</header>
-			<main>
-				<section id="profile">
-					<header>
+			<main class="layout__body">
+				<section class="cv-section cv-section--profile">
+					<header class="cv-section__header">
 						<h1>Profile</h1>
 					</header>
-					<p>A front-end web developer of complex websites with over ten years' experience.</p>
-					<ul>
-						<li>Extensive familiarity with both proprietary and open source languages.</li>
-						<li>Works to <abbr title="World Wide Web Consortium">W3C</abbr> standards.</li>
-						<li>Experienced in meeting client requirements.</li>
-						<li>Able to work with designers, other developers or solo as required.</li>
-					</ul>
+					<div class="cv-section__body">
+						<p>A front-end web developer of complex websites with over ten years' experience.</p>
+						<ul class="cv-section__list--unordered">
+							<li>Extensive familiarity with both proprietary and open source languages.</li>
+							<li>Works to <abbr title="World Wide Web Consortium">W3C</abbr> standards.</li>
+							<li>Experienced in meeting client requirements.</li>
+							<li>Able to work with designers, other developers or solo as required.</li>
+						</ul>
+					</div>
 				</section>
-				<section id="keySkills">
-					<header>
+				<section class="cv-section cv-section--skills">
+					<header class="cv-section__header">
 						<h1>Key Skills</h1>
 					</header>
-					<dl>
-						<dt>Primary</dt>
-						<dd>
-							<abbr title="HyperText Markup Language">HTML</abbr>,
-							<abbr title="Cascading Style Sheets">CSS</abbr>,
-							LESS,
-							JavaScript (including jQuery),
-							<abbr title="Extensible Markup Language">XML</abbr>,
-							<abbr title="EXtensible Stylesheet Language">XSL</abbr> &amp;
-							<abbr title="Extensible Stylesheet Language Formatting Objects">XSL-FO</abbr>
-						</dd>
-						<dt>Secondary</dt>
-						<dd>
-							.NET (C#),
-							PHP,
-							<abbr title="Structured Query Language">SQL</abbr>,
-							<abbr title="JavaServer Pages">JSP</abbr> &amp;
-							<abbr title="JavaServer Pages Standard Tag Library">JSTL</abbr>
-						</dd>
-					</dl>
+					<div class="cv-section__body">
+						<dl class="cv-section__list--definition">
+							<dt>Primary</dt>
+							<dd>
+								<abbr title="HyperText Markup Language">HTML</abbr>,
+								<abbr title="Cascading Style Sheets">CSS</abbr>,
+								LESS,
+								JavaScript (including jQuery),
+								<abbr title="Extensible Markup Language">XML</abbr>,
+								<abbr title="EXtensible Stylesheet Language">XSL</abbr> &amp;
+								<abbr title="Extensible Stylesheet Language Formatting Objects">XSL-FO</abbr>
+							</dd>
+							<dt>Secondary</dt>
+							<dd>
+								.NET (C#),
+								PHP,
+								<abbr title="Structured Query Language">SQL</abbr>,
+								<abbr title="JavaServer Pages">JSP</abbr> &amp;
+								<abbr title="JavaServer Pages Standard Tag Library">JSTL</abbr>
+							</dd>
+						</dl>
+					</div>
 				</section>
-				<section id="employmentHistory">
-					<header>
+				<section class="cv-section cv-section--jobs">
+					<header class="cv-section__header">
 						<h1>Employment History</h1>
 					</header>
-					<section>
-						<header>
-							<h1>Morningstar</h1>
-							<p>
-								<span class="job-title">Senior Web Developer</span>
-								<span class="job-period">2008 - Present</span>
-							</p>
-						</header>
-						<div>
-							<ul>
-								<li>The only dedicated front end developer in a team of back end developers spread across Europe and Asia who build customisable tools that can be seen on the websites of various financial institutions including Banks, Fund Managers and Advisors as well as our in-house European retail websites.</li>
-								<li>Responsible for re-architecting the real-time creation of the IWT team’s factsheets. The newer versions have greater functionality, are more flexible and a smaller code footprint than their predecessors. As a result it is now quicker and easier for the implementation managers to customise them to fit client requirements - or for developers to add further functionality. To aid all parties, documentation explaining the approach and all of the available options was written for the internal wiki.</li>
-								<li>Refactored, one module at a time, the CSS for the entire suite of modules that existed at that time. To ensure that these changes caused as little disruption as possible to existing clients, guidelines were provided to project managers detailing what to look out for during testing and what modifications were required. The result was a much reduced amount of CSS within the platform and made client customisation simpler.</li>
-								<li>The go-to person for many colleagues when they require advice on whether something is possible or encounter an issue (whether front- or back-end) which they are struggling with.</li>
-								<li>Ongoing responsibility for peer reviewing front end code (HTML, CSS, JavaScript and XSL) work produced by colleagues, training new team members on the factsheets, mentoring junior developers and assisting implementation managers with technical queries.</li>
-							</ul>
-						</div>
-					</section>
-					<section>
-						<header>
-							<h1>Hemscott</h1>
-							<p>
-								<span class="job-title">Senior Web Developer</span>
-								<span class="job-period">2006 - 2008</span>
-							</p>
-						</header>
-						<div>
-							<ul>
-								<li>Rebuilt, together with a small team of other web developers, two of the company's main websites. The result was a brace of modern websites, both in terms of technology and development techniques, each with a fresh look and feel which improved the user experience considerably when compared against their predecessors.</li>
-								<li>Constructed micro-sites for clients, customising the tools to meet style guidelines which ensured a consistent look when they were included within the customers' websites.</li>
-							</ul>
-						</div>
-					</section>
-					<section>
-						<header>
-							<h1>Westcliff Data Services</h1>
-							<p>
-								<span class="job-title">Web Developer</span>
-								<span class="job-period">2003 - 2005</span>
-							</p>
-						</header>
-						<div>
-							<p>Responsible for the construction of employer's CMS and development of online testing solutions. Modified clients' websites and assisted the in-house software developer.</p>
-						</div>
-					</section>
-					<section>
-						<header>
-							<h1>Heritage &amp; Co</h1>
-							<p>
-								<span class="job-title">Freelance Web Developer</span>
-								<span class="job-period">2003</span>
-							</p>
-						</header>
-						<div>
-							<p>Created a website that could present the company’s range of foreign properties to the wider audience.</p>
-						</div>
-					</section>
-					<section>
-						<header>
-							<h1>Accurate plc</h1>
-							<p>
-								<span class="job-title">Web Developer</span>
-								<span class="job-period">2002 - 2003</span>
-							</p>
-						</header>
-						<div>
-							<p>Built the website for the company’s security team as well as the company intranet, including a CMS. Added functionality to the company's main e-commerce website.</p>
-						</div>
-					</section>
-					<section>
-						<header>
-							<h1>Rebus IS</h1>
-							<p>
-								<span class="job-title">Developer</span>
-								<span class="job-period">2002</span>
-							</p>
-						</header>
-						<div>
-							<p>Created standalone software using VB v3 and v5 in association with MS Access databases.</p>
-						</div>
-					</section>
-					<section>
-						<header>
-							<h1>Pharmarcia Ltd</h1>
-							<p>
-								<span class="job-title">Financial Solutions Developer</span>
-								<span class="job-period">2001 - 2002</span>
-							</p>
-						</header>
-						<div>
-							<p>Worked with the finance director to keep track of the accounts from various Middle Eastern offices with via a MIS.</p>
-						</div>
-					</section>
-					<section>
-						<header>
-							<h1>Oakley Internet</h1>
-							<p>
-								<span class="job-title">Junior Web Developer</span>
-								<span class="job-period">2001</span>
-							</p>
-						</header>
-						<div>
-							<p>Developed, alongside senior colleagues, a range of e-commerce solutions that met the client’s needs.</p>
-						</div>
-					</section>
+						<div class="cv-section__body">
+						<section class="job">
+							<header class="job__header">
+								<h1 class="job--employer">Morningstar</h1>
+								<span class="job--title">Senior Web Developer</span>
+								<span class="job--period">2008 - Present</span>
+							</header>
+							<div class="job--about">
+								<ul class="cv-section__list--unordered">
+									<li>The only dedicated front end developer in a team of back end developers spread across Europe and Asia who build customisable tools that can be seen on the websites of various financial institutions including Banks, Fund Managers and Advisors as well as our in-house European retail websites.</li>
+									<li>Responsible for re-architecting the real-time creation of the IWT team’s factsheets. The newer versions have greater functionality, are more flexible and a smaller code footprint than their predecessors. As a result it is now quicker and easier for the implementation managers to customise them to fit client requirements - or for developers to add further functionality. To aid all parties, documentation explaining the approach and all of the available options was written for the internal wiki.</li>
+									<li>Refactored, one module at a time, the CSS for the entire suite of modules that existed at that time. To ensure that these changes caused as little disruption as possible to existing clients, guidelines were provided to project managers detailing what to look out for during testing and what modifications were required. The result was a much reduced amount of CSS within the platform and made client customisation simpler.</li>
+									<li>The go-to person for many colleagues when they require advice on whether something is possible or encounter an issue (whether front- or back-end) which they are struggling with.</li>
+									<li>Ongoing responsibility for peer reviewing front end code (HTML, CSS, JavaScript and XSL) work produced by colleagues, training new team members on the factsheets, mentoring junior developers and assisting implementation managers with technical queries.</li>
+								</ul>
+							</div>
+						</section>
+						<section class="job">
+							<header class="job__header">
+								<h1 class="job--employer">Hemscott</h1>
+								<span class="job--title">Web Developer/Senior Web Developer</span>
+								<span class="job--period">2006 - 2008</span>
+							</header>
+							<div class="job--about">
+								<ul class="cv-section__list--unordered">
+									<li>Rebuilt, together with a small team of other web developers, two of the company's main websites. The result was a brace of modern websites, both in terms of technology and development techniques, each with a fresh look and feel which improved the user experience considerably when compared against their predecessors.</li>
+									<li>Constructed micro-sites for clients, customising the tools to meet style guidelines which ensured a consistent look when they were included within the customers' websites.</li>
+								</ul>
+							</div>
+						</section>
+						<section class="job">
+							<header class="job__header">
+								<h1 class="job--employer">Westcliff Data Services</h1>
+								<span class="job--title">Web Developer</span>
+								<span class="job--period">2003 - 2005</span>
+							</header>
+							<div class="job--about">
+								<p>Responsible for the construction of employer's CMS and development of online testing solutions. Modified clients' websites and assisted the in-house software developer.</p>
+							</div>
+						</section>
+						<section class="job">
+							<header class="job__header">
+								<h1 class="job--employer">Heritage &amp; Co</h1>
+								<span class="job--title">Freelance Web Developer</span>
+								<span class="job--period">2003</span>
+							</header>
+							<div class="job--about">
+								<p>Created a website that could present the company’s range of foreign properties to the wider audience.</p>
+							</div>
+						</section>
+						<section class="job">
+							<header class="job__header">
+								<h1 class="job--employer">Accurate plc</h1>
+								<span class="job--title">Web Developer</span>
+								<span class="job--period">2002 - 2003</span>
+							</header>
+							<div class="job--about">
+								<p>Built the website for the company’s security team as well as the company intranet, including a CMS. Added functionality to the company's main e-commerce website.</p>
+							</div>
+						</section>
+						<section class="job">
+							<header class="job__header">
+								<h1 class="job--employer">Rebus IS</h1>
+								<span class="job--title">Developer</span>
+								<span class="job--period">2002</span>
+							</header>
+							<div class="job--about">
+								<p>Created standalone software using VB v3 and v5 in association with MS Access databases.</p>
+							</div>
+						</section>
+						<section class="job">
+							<header class="job__header">
+								<h1 class="job--employer">Pharmarcia Ltd</h1>
+								<span class="job--title">Financial Solutions Developer</span>
+								<span class="job--period">2001 - 2002</span>
+							</header>
+							<div class="job--about">
+								<p>Worked with the finance director to keep track of the accounts from various Middle Eastern offices with via a MIS.</p>
+							</div>
+						</section>
+						<section class="job">
+							<header class="job__header">
+								<h1 class="job--employer">Oakley Internet</h1>
+								<span class="job--title">Junior Web Developer</span>
+								<span class="job--period">2001</span>
+							</header>
+							<div class="job--about">
+								<p>Developed, alongside senior colleagues, a range of e-commerce solutions that met the client’s needs.</p>
+							</div>
+						</section>
+					</div>
 				</section>
-				<section id="portfolio">
-					<header>
+				<section class="cv-section cv-section--portfolio">
+					<header class="cv-section__header">
 						<h1>Portfolio</h1>
 					</header>
-					<ul>
-						<li>
-							<a rel="nofollow noreferrer" href="http://lt.morningstar.com/demo/">Morningstar Integrated Web Tools</a>
-						</li>
-						<li>
-							<a rel="nofollow noreferrer" href="http://ias-av.com/">Interactive Audio Visual Solutions</a>
-						</li>
-						<li>
-							<a rel="nofollow noreferrer" href="http://www.foley-oneill.com/">Foley O'Neill</a>
-						</li>
-						<li>
-							<a rel="nofollow noreferrer" href="https://github.com/MisanthropeGirl">GitHub</a>
-						</li>
-					</ul>
+					<div class="cv-section__body">
+						<ul class="cv-section__list--unordered">
+							<li>
+								<a rel="nofollow noreferrer" href="http://lt.morningstar.com/demo/">Morningstar Integrated Web Tools</a>
+							</li>
+							<li>
+								<a rel="nofollow noreferrer" href="http://ias-av.com/">Interactive Audio Visual Solutions</a>
+							</li>
+							<li>
+								<a rel="nofollow noreferrer" href="http://www.foley-oneill.com/">Foley O'Neill</a>
+							</li>
+							<li>
+								<a rel="nofollow noreferrer" href="https://github.com/MisanthropeGirl">GitHub</a>
+							</li>
+						</ul>
+					</div>
 				</section>
-				<section id="education">
-					<header>
+				<section class="cv-section cv-section--education">
+					<header class="cv-section__header">
 						<h1>Education</h1>
 					</header>
-					<p>BEng in Electronic Engineering (Hons)</p>
-					<p>Mathematics, Physics and Chemistry A-Levels</p>
+					<div class="cv-section__body">
+						<p>BEng in Electronic Engineering (Hons)</p>
+						<p>Mathematics, Physics and Chemistry A-Levels</p>
+					</div>
 				</section>
 			</main>
-			<footer>
-				<ul>
-					<li id="pdf">
-						<a rel="nofollow" href="resources/pdf/CV.pdf" title="Download PDF version">
+			<footer class="layout__footer">
+				<ul class="additional-information">
+					<li class="additional-information__item">
+						<a rel="nofollow" href="resources/pdf/CV.pdf" class="additional-information__link" title="Download PDF version">
 							<img src="resources/images/pdf.png" alt="PDF" />
 						</a>
 					</li>
-					<li id="email">
-						<a rel="nofollow" href="mailto:jobs@clarissaclement.me.uk" title="Contact Me">
+					<li class="additional-information__item">
+						<a rel="nofollow" href="mailto:jobs@clarissaclement.me.uk" class="additional-information__link" title="Contact Me">
 							<img src="resources/images/email.png" alt="Email" />
 						</a>
 					</li>
-					<li id="linkedin">
-						<a rel="nofollow" href="http://uk.linkedin.com/in/clarissaclement" title="LinkedIn">
+					<li class="additional-information__item">
+						<a rel="nofollow" href="http://uk.linkedin.com/in/clarissaclement" class="additional-information__link" title="LinkedIn">
 							<img src="resources/images/linkedin.png" alt="LinkedIn" />
 						</a>
 					</li>
